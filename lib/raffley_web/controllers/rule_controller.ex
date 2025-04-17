18c4fee282cc839e5fee3aa90893b189d97a9278
Jamie Curle <1866+treejamie@users.noreply.github.com>
2025-04-17 17:52:52 +0100

    5 - Server Side Rendering with HEEx (#64)
    Added a rule controller, a rule view, a rule template and a rule logic file. We assigned the rules into the conn and rendered it out using a comprehension.

---
+++ lib/raffley_web/controllers/rule_controller.ex
@@ -0,0 +1,14 @@
+defmodule RaffleyWeb.RuleController do
+  use RaffleyWeb, :controller
+
+  alias Raffley.Rules
+
+  def index(conn, _params) do
+    emojis = ~w(🐞 🤢  🔥) |> Enum.random() |> String.duplicate(5)
+
+    rules = Rules.list_rules()
+
+    render(conn, :index, emojis: emojis, rules: rules)
+  end
+
+end
---
+++ lib/raffley_web/controllers/rule_html.ex
@@ -0,0 +1,6 @@
+defmodule RaffleyWeb.RuleHTML do
+  use RaffleyWeb, :html
+
+  embed_templates "rule_html/*"
+
+end
