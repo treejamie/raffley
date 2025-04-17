d96c2ef36893a35cfc31768df59c9aa84d20637b
treejamie <me@treejamie.com>
2025-04-17 17:50:34 +0100

    Added a rule controller, a rule view, a rule template and a rule logic file. We assigned the rules into the conn

---
+++ lib/raffley/rules.ex
@@ -0,0 +1,20 @@
+defmodule Raffley.Rules do
+
+  def list_rules do
+    [
+      %{
+        id: 1,
+        text: "Participants must have a high tolerance for puns and dad jokes.
+        🙃"
+      },
+      %{
+        id: 2,
+        text: "Winner must do a victory dance when claimning their prize. 🕺"
+      },
+      %{
+        id: 3,
+        text: "Have fun! 🎟️"
+      }
+    ]
+  end
+end
