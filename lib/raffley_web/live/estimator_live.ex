af0ebf51e1e5e80c5d08bf8bf4000ad7ffb23d56
treejamie <me@treejamie.com>
2025-04-22 16:03:52 +0100

    Added in some basic interactivity into the live view.

--- lib/raffley_web/live/estimator_live.ex
+++ lib/raffley_web/live/estimator_live.ex
@@ -12,6 +12,9 @@ defmodule RaffleyWeb.EstimatorLive do
     <div class="estimator">
       <h1>Raffle Estimator</h1>
       <section>
+        <button phx-click="add" phx-value-quantity="5">
+        +
+        </button>
         <div>
           <%= @tickets %>
         </div>
@@ -22,5 +25,10 @@ defmodule RaffleyWeb.EstimatorLive do
       </section>
     </div>
     """
+  end
+
+  def handle_event("add", %{"quantity" => quantity}, socket) do
+    socket = update(socket, :tickets, &(&1 + String.to_integer(quantity)))
+    {:noreply, socket}
   end
 end
