fce195d5579f56954c8cdd8405a1746f2fdce491
Tree Jamie <me@treejamie.com>
2025-04-22 21:37:54 +0100

    16 - Slots. Slots are totally new as are components.

--- lib/raffley_web/components/custom_components.ex
+++ lib/raffley_web/components/custom_components.ex
@@ -19,4 +19,24 @@ defmodule RaffleyWeb.CustomComponents do
     """
   end
 
+
+  slot :inner_block, required: true
+  slot :details
+
+  def banner(assigns) do
+
+    assigns = assign(assigns, :emoji, ~w(😎 🤩 😶) |> Enum.random())
+
+    ~H"""
+      <div class="banner">
+        <h1>
+          <%= render_slot(@inner_block) %>
+        </h1>
+        <div :for={details <- @details} class="details">
+          <%= render_slot(details, @emoji ) %>
+        </div>
+      </div>
+    """
+  end
+
 end
--- lib/raffley_web/live/raffle_live/index.ex
+++ lib/raffley_web/live/raffle_live/index.ex
@@ -12,12 +12,25 @@ defmodule RaffleyWeb.RaffleLive.Index do
   def render(assigns) do
     ~H"""
     <div class="raffle-index">
+      <.banner >
+        <.icon name="hero-sparkles-solid" />
+        Mystery Raffle Coming Soon
+
+        <:details :let={vibe}>
+          To Be Revealed Tomorrow
+          <%= vibe %>
+        </:details>
+        <:details>
+          Any guesses?
+        </:details>
+      </.banner>
       <div class="raffles">
         <.raffle_card :for={raffle <- @raffles} raffle={raffle} />
       </div>
     </div>
     """
   end
+
 
   attr :raffle, Raffley.Raffle, required: true
 
@@ -35,6 +48,4 @@ defmodule RaffleyWeb.RaffleLive.Index do
   </div>
   """
   end
-
-
 end
