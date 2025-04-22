c1498a33b44348f1f8ca11ff4a358f96ec598f77
Tree Jamie <me@treejamie.com>
2025-04-22 21:03:36 +0100

    chapter 12, 13 and 14

--- lib/raffley_web/live/estimator_live.ex
+++ lib/raffley_web/live/estimator_live.ex
@@ -2,6 +2,11 @@ defmodule RaffleyWeb.EstimatorLive do
   use RaffleyWeb, :live_view
 
   def mount(_params, _session, socket) do
+    # send message only when mounted
+    if connected?(socket) do
+      Process.send_after(self(), :tick, 2000)
+    end
+
     socket = assign(socket, tickets: 0, price: 3 )
 
     IO.inspect(self(), label: "MOUNT")
@@ -44,5 +49,10 @@ defmodule RaffleyWeb.EstimatorLive do
   def handle_event("set-price", %{"price" => price}, socket) do
     socket = assign(socket, :price, String.to_integer(price))
     {:noreply, socket}
+  end
+
+  def handle_info(:tick, socket) do
+    Process.send_after(self(), :tick, 2000)
+    {:noreply, update(socket, :tickets, &(&1 +10))}
   end
 end
---
+++ lib/raffley_web/live/raffle_live/index.ex
@@ -0,0 +1,48 @@
+defmodule RaffleyWeb.RaffleLive.Index do
+  use RaffleyWeb, :live_view
+  alias Raffley.Raffles
+
+  def mount(_params, _session, socket) do
+    socket = assign(socket, :raffles, Raffles.list_raffles())
+    {:ok, socket}
+  end
+
+
+  def render(assigns) do
+    ~H"""
+    <div class="raffle-index">
+      <div class="raffles">
+        <.raffle_card :for={raffle <- @raffles} raffle={raffle} />
+      </div>
+    </div>
+    """
+  end
+
+  attr :raffle, Raffley.Raffle, required: true
+
+  def raffle_card(assigns) do
+    ~H"""
+    <div  class="card">
+    <img src={@raffle.image_path}/>
+    <h2> <%= @raffle.prize %></h2>
+    <div class="details">
+      <div class="price">
+        $<%= @raffle.ticket_price %>
+      </div>
+      <.badge status={@raffle.status} />
+    </div>
+  </div>
+  """
+  end
+
+  attr :status, :atom, values: [:upcoming, :open, :closed], default: :upcoming
+
+  def badge(assigns) do
+    ~H"""
+      <div class="badge">
+      <%= @status %>
+    </div>
+    """
+  end
+
+end
