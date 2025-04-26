cf57a943209e2309dbf0f70e95648d8b29680285
treejamie <me@treejamie.com>
2025-04-26 16:49:17 +0100

    now using streams

--- lib/raffley_web/live/raffle_live/index.ex
+++ lib/raffley_web/live/raffle_live/index.ex
@@ -4,7 +4,7 @@ defmodule RaffleyWeb.RaffleLive.Index do
   import RaffleyWeb.CustomComponents
 
   def mount(_params, _session, socket) do
-    socket = assign(socket, :raffles, Raffles.list_raffles())
+    socket = stream(socket, :raffles, Raffles.list_raffles())
     {:ok, socket}
   end
 
@@ -24,8 +24,8 @@ defmodule RaffleyWeb.RaffleLive.Index do
           Any guesses?
         </:details>
       </.banner>
-      <div class="raffles">
-        <.raffle_card :for={raffle <- @raffles} raffle={raffle} />
+      <div class="raffles" id="raffles" phx-update="stream">
+        <.raffle_card :for={{dom_id, raffle} <- @streams.raffles} raffle={raffle} id={dom_id}/>
       </div>
     </div>
     """
@@ -33,10 +33,11 @@ defmodule RaffleyWeb.RaffleLive.Index do
 
 
   attr :raffle, Raffley.Raffles.Raffle, required: true
+  attr :id, :string, required: true
 
   def raffle_card(assigns) do
     ~H"""
-    <.link navigate={~p"/raffles/#{@raffle}"}>
+    <.link navigate={~p"/raffles/#{@raffle}"} id={@id}>
       <div class="card">
         <img src={@raffle.image_path}/>
         <h2> <%= @raffle.prize %></h2>
