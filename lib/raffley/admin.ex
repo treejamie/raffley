2200cf60d022ae397c482a764202c79867edbb14
treejamie <me@treejamie.com>
2025-04-30 09:13:56 +0100

    Raffles are being created but they are not going through Ecto changesets, so this code should be treated as hostile

--- lib/raffley/admin.ex
+++ lib/raffley/admin.ex
@@ -9,4 +9,23 @@ defmodule Raffley.Admin do
     |> order_by(desc: :inserted_at)
     |> Repo.all()
   end
+
+  def create_raffle(attrs \\ %{} ) do
+    #
+    #
+    #
+    #  WARNING - THIS LESSON WAS TO PROVE THE POINT ABOUT ECTO CHANGESETS
+    #            BEING A THING TO USE. THIS CODE DOES NOT USE CHANGESETS.
+    #
+    # . DRAGONS BELOW. DRAGONS!
+    %Raffle{
+      prize: attrs["prize"],
+      description: attrs["description"],
+      ticket_price: attrs["ticket_price"] |> String.to_integer(),
+      status: attrs["status"] |> String.to_existing_atom,
+      image_path: "images/jersey.jpg"
+    }
+    |> Repo.insert!()
+
+  end
 end
