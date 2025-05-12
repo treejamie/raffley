82ca6c962ac2f57c7240e37a0d02013b670da024
Jamie Curle <1866+treejamie@users.noreply.github.com>
2025-05-12 21:42:36 +0100

    53 - Many To Many Forms (#104)
    Now saving tickets with many to many relationships

--- lib/raffley_web/live/raffle_live/show.ex
+++ lib/raffley_web/live/raffle_live/show.ex
@@ -1,12 +1,17 @@
 defmodule RaffleyWeb.RaffleyLive.Show do
   use RaffleyWeb, :live_view
   alias Raffley.Raffles
+  alias Raffley.Tickets
+  alias Raffley.Tickets.Ticket
+
+  import RaffleyWeb.CustomComponents
 
   on_mount({RaffleyWeb.UserAuth, :mount_current_user})
 
   def mount(_params, _session, socket) do
-    socket =
-      assign(socket, :form, to_form(%{}))
+    changeset = Tickets.change_ticket(%Ticket{})
+
+    socket = assign(socket, :form, to_form(changeset))
 
     {:ok, socket}
   end
@@ -53,7 +58,8 @@ defmodule RaffleyWeb.RaffleyLive.Show do
         <div class="left">
           <div :if={@raffle.status == :open}>
             <%= if @current_user do %>
-            <.form for={@form} id="ticket-form">
+            <.form for={@form} id="ticket-form" phx-change="validate"
+              phx-submit="save">
               <.input field={@form[:comment]} placeholder="Comment.." autofocus />
               <.button>
                 Get a ticket
@@ -109,4 +115,29 @@ defmodule RaffleyWeb.RaffleyLive.Show do
     </section>
     """
   end
+
+  def handle_event("validate", %{"ticket" => ticket_params}, socket) do
+    changeset = Tickets.change_ticket(%Ticket{}, ticket_params)
+
+    socket = assign(socket, :form, to_form(changeset, action: :validate))
+    {:noreply, socket}
+  end
+
+  def handle_event("save", %{"ticket" => ticket_params}, socket) do
+
+    %{raffle: raffle, current_user: user} = socket.assigns
+
+    case Tickets.create_ticket(raffle, user, ticket_params) do
+      {:ok, _ticket} ->
+        changeset = Tickets.change_ticket(%Ticket{})
+        socket = assign(socket, :form, to_form(changeset))
+        {:noreply, socket}
+
+      {:error, changeset} ->
+        socket = assign(socket, :form, to_form(changeset))
+        {:noreply, socket}
+      end
+  end
+
+
 end
